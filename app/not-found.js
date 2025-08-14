7138a5f7910f245c93196be8349ebca8c280a8af
Matthew Aitken <matt@itken.net>
2025-08-14 14:09:46 +0100

    Add global not-found.js

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+            <main className="not-found">
+                <h1>Not Found!</h1>
+                <p>This is not the page you were looking for.</p>
+            </main>
+    );
+}
